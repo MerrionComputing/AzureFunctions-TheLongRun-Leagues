3fb39eff19734ee734a181ae5957c2a99e482fab
MerrionComputing <Duncan@merrioncomputing.com>
2018-05-21 14:09:34 +0100

    Projection to get the classifier states for a [Query]

---
+++ TheLongRun.Common/Events/Query/Projections/Query_Classifications_Projection.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using CQRSAzure.EventSourcing;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json.Linq;
+using TheLongRun.Common.Events.Query;
+
+namespace TheLongRun.Common.Events.Query.Projections
+{
+    /// <summary>
+    /// A projection to get the state of the classifiers running for any given query
+    /// </summary>
+    public class Query_Classifications_Projection
+        : CQRSAzure.EventSourcing.ProjectionBaseUntyped,
+        CQRSAzure.EventSourcing.IHandleEvent<ClassificationRequested>,
+        CQRSAzure.EventSourcing.IHandleEvent<ClassificationRunStarted>,
+        CQRSAzure.EventSourcing.IHandleEvent<ClassificationResultReturned>,
+        IProjectionUntyped
+    {
+
+
+        #region Private members
+        private TraceWriter log = null;
+        private List<string> keysToBeClassified = new List<string>();
+        private List<string> keysBeingClassified = new List<string>();
+        private Dictionary<string, UInt32> keysIncluded = new Dictionary<string, uint>();
+        private Dictionary<string, UInt32> keysExcluded = new Dictionary<string, uint>();
+        #endregion
+
+        /// <summary>
+        /// It does not make sense for this projection to support snapshots 
+        /// </summary>
+        public override bool SupportsSnapshots => false ;
+
+        public override void HandleEvent<TEvent>(TEvent eventToHandle)
+        {
+
+            #region Logging
+            if (null != log)
+            {
+                log.Verbose($"HandleEvent<{ typeof(TEvent).FullName  }>())",
+                    nameof(Query_Classifications_Projection));
+            }
+            #endregion
+
+            if (eventToHandle.GetType() == typeof(ClassificationRequested ))
+            {
+                HandleEvent(eventToHandle as ClassificationRequested);
+            }
+
+            if (eventToHandle.GetType() == typeof(ClassificationRunStarted))
+            {
+                HandleEvent(eventToHandle as ClassificationRunStarted);
+            }
+
+            if (eventToHandle.GetType() == typeof(ClassificationResultReturned))
+            {
+                HandleEvent(eventToHandle as ClassificationResultReturned);
+            }
+
+        }
+
+        public  void HandleEvent(ClassificationRequested eventHandled)
+        {
+            if (null != eventHandled)
+            {
+                #region Logging
+                if (null != log)
+                {
+                    log.Verbose($"Query classification requested  {eventHandled.ClassificationTypeName   } for { eventHandled.DomainName }.{eventHandled.AggregateType}.{eventHandled.AggregateInstanceKey } as at {eventHandled.AsOfDate} ",
+                        nameof(Query_Classifications_Projection));
+                }
+                #endregion
+                // add this to the unprocessed list
+                keysToBeClassified.Add(eventHandled.AggregateInstanceKey);
+            }
+        }
+
+        public void HandleEvent(ClassificationRunStarted eventHandled)
+        {
+            if (null != eventHandled)
+            {
+                #region Logging
+                if (null != log)
+                {
+                    log.Verbose($"Query classification run started  {eventHandled.ClassificationTypeName  } for { eventHandled.DomainName }.{eventHandled.AggregateType}.{eventHandled.AggregateInstanceKey } as at {eventHandled.AsOfDate} ",
+                        nameof(Query_Classifications_Projection));
+                }
+                #endregion
+
+                // remove this from this to the unprocessed list
+                keysToBeClassified.Remove(eventHandled.AggregateInstanceKey);
+
+                // add it to the in-flight list
+                keysBeingClassified.Add(eventHandled.AggregateInstanceKey);
+            }
+        }
+
+        public void HandleEvent(ClassificationResultReturned eventHandled)
+        {
+            if (null != eventHandled)
+            {
+                #region Logging
+                if (null != log)
+                {
+                    log.Verbose($"Query classification results returned for  {eventHandled.ClassificationTypeName  } for { eventHandled.DomainName }.{eventHandled.AggregateType}.{eventHandled.AggregateInstanceKey } as at {eventHandled.AsOfDate} ",
+                        nameof(Query_Classifications_Projection));
+                }
+                #endregion
+
+                // remove this from this to the unprocessed list - in case it never got into the in-flight set
+                keysToBeClassified.Remove(eventHandled.AggregateInstanceKey);
+
+                // remove this from the in-flight list
+                keysBeingClassified.Remove (eventHandled.AggregateInstanceKey);
+
+                if (eventHandled.IsInGroup)
+                {
+                    keysExcluded.Remove(eventHandled.AggregateInstanceKey);
+                    keysIncluded.Add(eventHandled.AggregateInstanceKey, eventHandled.AsOfSequenceNumber); 
+                }
+                else
+                {
+                    keysIncluded.Remove(eventHandled.AggregateInstanceKey);
+                    keysExcluded.Add(eventHandled.AggregateInstanceKey, eventHandled.AsOfSequenceNumber);
+                }
+            }
+        }
+
+        public override void HandleEventJSon(string eventFullName, JObject eventToHandle)
+        {
+            #region Logging
+            if (null != log)
+            {
+                log.Verbose($"HandleEventJSon({eventFullName})",
+                    nameof(Query_Classifications_Projection));
+            }
+            #endregion
+
+
+            if (eventFullName == typeof(ClassificationRequested).FullName)
+            {
+                HandleEvent<ClassificationRequested>(eventToHandle.ToObject<ClassificationRequested>());
+            }
+
+            if (eventFullName == typeof(ClassificationRunStarted ).FullName)
+            {
+                HandleEvent<ClassificationRunStarted>(eventToHandle.ToObject<ClassificationRunStarted>());
+            }
+
+            if (eventFullName == typeof(ClassificationResultReturned).FullName)
+            {
+                HandleEvent<ClassificationResultReturned>(eventToHandle.ToObject<ClassificationResultReturned>());
+            }
+        }
+
+        public override bool HandlesEventType(Type eventType)
+        {
+            return HandlesEventTypeByName(eventType.FullName);
+        }
+
+        public override bool HandlesEventTypeByName(string eventTypeFullName)
+        {
+
+            #region Logging
+            if (null != log)
+            {
+                log.Verbose($"HandlesEventTypeByName({eventTypeFullName})",
+                    nameof(Query_Projections_Projection));
+            }
+            #endregion
+
+
+            if (eventTypeFullName == typeof(ClassificationRequested).FullName)
+            {
+                return true;
+            }
+
+            if (eventTypeFullName == typeof(ClassificationResultReturned).FullName)
+            {
+                return true;
+            }
+
+            if (eventTypeFullName == typeof(ClassificationResultReturned).FullName)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public Query_Classifications_Projection(TraceWriter logIn = null)
+        {
+            if (null != logIn)
+            {
+                log = logIn;
+            }
+        }
+    }
+}
--- TheLongRun.Common/Events/Query/Projections/Query_Projections_Projection.cs
+++ TheLongRun.Common/Events/Query/Projections/Query_Projections_Projection.cs
@@ -15,6 +15,7 @@ namespace TheLongRun.Common.Events.Query.Projections
     public class Query_Projections_Projection
         : CQRSAzure.EventSourcing.ProjectionBaseUntyped,
         CQRSAzure.EventSourcing.IHandleEvent<ProjectionRequested >,
+        CQRSAzure.EventSourcing.IHandleEvent<ProjectionRunStarted >,
         CQRSAzure.EventSourcing.IHandleEvent<ProjectionValueReturned >,
         IProjectionUntyped
     {
@@ -22,6 +23,7 @@ namespace TheLongRun.Common.Events.Query.Projections
         #region Private members
         private TraceWriter log = null;
         private List<ProjectionRequested> unprocessedRequests = new List<ProjectionRequested>();
+        private List<ProjectionRunStarted> inflightRequests = new List<ProjectionRunStarted>();
         private List<ProjectionValueReturned> processedRequests = new List<ProjectionValueReturned>();
         #endregion
 
@@ -33,6 +35,17 @@ namespace TheLongRun.Common.Events.Query.Projections
             get
             {
                 return unprocessedRequests.AsReadOnly();
+            }
+        }
+
+        /// <summary>
+        /// The set of projection requests being run
+        /// </summary>
+        public IReadOnlyList<ProjectionRunStarted > RequestsInProgress
+        {
+            get
+            {
+                return inflightRequests.AsReadOnly();
             }
         }
 
@@ -102,6 +115,11 @@ namespace TheLongRun.Common.Events.Query.Projections
                 return true;
             }
 
+            if (eventTypeFullName == typeof(ProjectionRunStarted).FullName)
+            {
+                return true;
+            }
+
             return false;
         }
 
@@ -124,6 +142,11 @@ namespace TheLongRun.Common.Events.Query.Projections
             if (eventToHandle.GetType() == typeof(ProjectionValueReturned ))
             {
                 HandleEvent(eventToHandle as ProjectionValueReturned );
+            }
+
+            if (eventToHandle.GetType() == typeof(ProjectionRunStarted ))
+            {
+                HandleEvent(eventToHandle as ProjectionRunStarted);
             }
         }
 
@@ -176,14 +199,54 @@ namespace TheLongRun.Common.Events.Query.Projections
                 {
                     unprocessedRequests.RemoveAt(unprocessedIndex);
                 }
+
+                // remove it from any in-progress list
+                int inflightIndex = inflightRequests.FindIndex(f =>
+                f.DomainName == eventHandled.DomainName &&
+                f.AggregateType == eventHandled.AggregateType &&
+                f.AggregateInstanceKey == eventHandled.AggregateInstanceKey);
+
+                if (inflightIndex >= 0)
+                {
+                    inflightRequests.RemoveAt(inflightIndex);
+                }
+
                 // add it to the processed list
                 processedRequests.Add(eventHandled);
             }
         }
 
+        public  void HandleEvent(ProjectionRunStarted eventHandled)
+        {
+            if (null != eventHandled)
+            {
+                #region Logging
+                if (null != log)
+                {
+                    log.Verbose($"Query projection run started  {eventHandled.ProjectionTypeName  } for { eventHandled.DomainName }.{eventHandled.AggregateType}.{eventHandled.AggregateInstanceKey } as at {eventHandled.AsOfDate} ",
+                        nameof(Query_Projections_Projection));
+                }
+                #endregion
+
+                // remove this from this to the unprocessed list
+                int unprocessedIndex = unprocessedRequests.FindIndex(f =>
+                f.DomainName == eventHandled.DomainName &&
+                f.AggregateType == eventHandled.AggregateType &&
+                f.AggregateInstanceKey == eventHandled.AggregateInstanceKey);
+
+                if (unprocessedIndex >= 0)
+                {
+                    unprocessedRequests.RemoveAt(unprocessedIndex);
+                }
+
+                // add it to the in-flight requests list
+                inflightRequests.Add(eventHandled);
+            }
+        }
+
         public Query_Projections_Projection(TraceWriter logIn = null)
         {
-            if (null != logIn )
+            if (null != logIn)
             {
                 log = logIn;
             }
