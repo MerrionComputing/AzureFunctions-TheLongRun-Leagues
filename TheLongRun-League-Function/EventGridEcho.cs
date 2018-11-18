da4eb5397a0fcb0294b7df55a901d7de6b694c23
MerrionComputing <Duncan@merrioncomputing.com>
2018-11-18 17:09:13 +0000

    Update JSon version and CQRSAzure libs

---
+++ TheLongRun-League-Function/EventGridEcho.cs
@@ -0,0 +1,21 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+
+namespace TheLongRunLeaguesFunction
+{
+    public static class EventGridEcho
+    {
+        [FunctionName("EventGridEcho")]
+        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, 
+            ILogger log)
+        {
+            log.LogInformation(eventGridEvent.Data.ToString());
+        }
+    }
+}
